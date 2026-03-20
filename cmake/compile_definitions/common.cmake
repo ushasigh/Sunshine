0ff143096017b868e9291f0f2c86aac8a9492ce8
Yoshi-Anpan <yoshiyoshieiei@icloud.com>
2026-03-19 17:03:12 -0700

    Modifications for the video recording feature

--- cmake/compile_definitions/common.cmake
+++ cmake/compile_definitions/common.cmake
@@ -98,6 +98,8 @@ set(SUNSHINE_TARGET_FILES
         "${CMAKE_SOURCE_DIR}/src/video.h"
         "${CMAKE_SOURCE_DIR}/src/video_colorspace.cpp"
         "${CMAKE_SOURCE_DIR}/src/video_colorspace.h"
+        "${CMAKE_SOURCE_DIR}/src/video_recorder.cpp"
+        "${CMAKE_SOURCE_DIR}/src/video_recorder.h"
         "${CMAKE_SOURCE_DIR}/src/input.cpp"
         "${CMAKE_SOURCE_DIR}/src/input.h"
         "${CMAKE_SOURCE_DIR}/src/audio.cpp"
